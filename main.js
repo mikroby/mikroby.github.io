3448f019b2f4d04c976d3cea287c45ef8699ad91
mikroby <rtamas@hotmail.com>
2021-09-07 11:16:34 +0200

    update

--- main.js
+++ main.js
@@ -4,8 +4,10 @@ function newsLetter() {
 
 let clickAbleImage = document.querySelectorAll('.photos img');
 for (let i = 0; i < clickAbleImage.length; i++) {
-    clickAbleImage[i].onclick = function() {window.open(this.src)};
+    clickAbleImage[i].onclick = function () { window.open(this.src) };
 }
 
-let clickAbleText = document.querySelector('.navigation h2');
-clickAbleText.onclick = function() {location.assign("/index.html#pageTop")};
+let clickAbleElement = document.querySelectorAll('div[class*="header"]');
+for (let i = 0; i < clickAbleElement.length; i++) {
+    clickAbleElement[i].onclick = function () { location.assign("/index.html#pageTop") };
+}
