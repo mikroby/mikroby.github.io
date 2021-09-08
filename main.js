4a99dde66d7b6261b6f3810cd34d61d00278b9bd
mikroby <rtamas@hotmail.com>
2021-09-08 22:38:43 +0200

    012

--- main.js
+++ main.js
@@ -7,13 +7,8 @@ for (let i = 0; i < clickAbleImage.length; i++) {
     clickAbleImage[i].onclick = function () { window.open(this.src) };
 }
 
-/* let clickAbleElement = document.querySelectorAll('div[class*="header"]');
-for (let i = 0; i < clickAbleElement.length; i++) {
-    clickAbleElement[i].onclick = function () { location.assign("/index.html#pageTop") };
-} */
-
 let clickAbleElement = document.querySelector(".header1");
-clickAbleElement.onclick = function () { location.assign("/index.html#pageTop") };
+clickAbleElement.onclick = function () {location.assign("/index.html#pageTop")};
 
 clickAbleElement = document.querySelector(".header2");
-clickAbleElement.onclick = function () { window.open("https://hu.wikipedia.org/wiki/F%C3%BAzi%C3%B3s_zene") };
+clickAbleElement.onclick = function () {location.assign("/links.html")};
