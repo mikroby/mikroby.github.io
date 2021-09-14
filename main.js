118879f73140d8d8eb38f062eb34e3c602cfc265
mikroby <rtamas@hotmail.com>
2021-09-14 02:52:40 +0200

    1001

--- main.js
+++ main.js
@@ -6,13 +6,13 @@ function openNewWindow() {
     window.open(this.src);
 }
 
-// All image on the photo-page can be clickable and open in new window
+// All photos-classed images can be clickable and open in new window.
 let clickAbleImage = document.querySelectorAll('.photos img');
 for (let i = 0; i < clickAbleImage.length; i++) {
     clickAbleImage[i].onclick = openNewWindow;
 }
 
-// in all pages the headers can be clickable...
+// in all pages the headers can be clickable.
 let clickAbleHeader = document.querySelector(".header1");
 clickAbleHeader.onclick = function () { location.assign("/index.html#pageTop") };
 
