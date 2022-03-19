dc5904be787379916dae02e007b7259939a231d2
mikroby <rtamas@hotmail.com>
2022-03-19 23:06:34 +0100

    sensor added

--- sensor/app.js
+++ sensor/app.js
@@ -3,32 +3,37 @@
 // const sensorName = 'magnetometer'
 const sensorName = 'accelerometer'
 
-const statusText = document.querySelector('.status span')
+const state = document.querySelector('.status span')
 const xValue = document.querySelector('.x span')
 const yValue = document.querySelector('.y span')
 const zValue = document.querySelector('.z span')
 
 
+const useSensor = () => {
+  
+  // let sensor = new Magnetometer({ frequency: 1 })
+  let sensor = new Accelerometer({ frequency: 1 })
+
+  sensor.addEventListener('reading', e => {
+    xValue.textContent = sensor.x
+    yValue.textContent = sensor.y
+    zValue.textContent = sensor.z
+  })
+  sensor.addEventListener('error', event => {
+    state.textContent = `${event.error.name}, ${event.error.message}`
+  })
+
+  sensor.start()
+}
+
+
 navigator.permissions.query({ name: sensorName })
   .then(result => {
     if (result.state === 'denied') {
-      statusText.content = `Permission to use ${sensorName} sensor is denied.`
+      state.textContent = `Permission to use ${sensorName} sensor is denied.`
       return
     }
     // Use the sensor.
-
-    // let sensor = new Magnetometer({ frequency: 1 })
-    let sensor = new Accelerometer({ frequency: 1 })
-
-    sensor.addEventListener('reading', e => {
-      xValue.textContent = sensor.x
-      yValue.textContent = sensor.y
-      zValue.textContent = sensor.z
-    })
-    sensor.addEventListener('error', event => {
-      statusText.textContent = `${event.error.name} ${event.error.message}`
-    })
-
-    sensor.start()
+    useSensor()
   });
 
