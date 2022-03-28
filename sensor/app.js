1a353b966b7c5480c2c4576a34f2b797801901aa
mikroby <rtamas@hotmail.com>
2022-03-28 12:42:10 +0200

    more sensors added

--- sensor/app.js
+++ sensor/app.js
@@ -1,25 +1,28 @@
 'use strict'
 
-const sensors = ['accelerometer', 'magnetometer']
+const sensors = ['accelerometer', 'magnetometer', 'gyroscope', 'ambient-light-sensor']
 const sensorObject = {
   'magnetometer'() { return new Magnetometer({ frequency: 10 }) }
   ,
   'accelerometer'() { return new Accelerometer({ frequency: 10 }) }
+  ,
+  'gyroscope'() { return new Gyroscope() }
+  ,
+  'ambient-light-sensor'() { return new AmbientLightSensor() }
 }
 
 
 const useSensor = (sensorName, index, result) => {
   const template = `
     <div class="container">
-    <h1 class="sensor">${index}. Sensor : ${sensorName}</h1>
-    <p>${result.state}</p>
+    <h2 class="sensor">${index}. Sensor : ${sensorName}</h2>
+    <small>${result.state}</small>
     <p class="status index${index}">Status : <span></span></p>  
     <p class="x index${index}">Value along X-axis : <span></span></p>
     <p class="y index${index}">Value along Y-axis : <span></span></p>
     <p class="z index${index}">Value along Z-axis : <span></span></p>    
     </div>
     `
-
   document.body.insertAdjacentHTML('beforeend', template)
 
   const state = document.querySelector(`.status.index${index} span`)
@@ -27,28 +30,34 @@ const useSensor = (sensorName, index, result) => {
   const yValue = document.querySelector(`.y.index${index} span`)
   const zValue = document.querySelector(`.z.index${index} span`)
 
-  
+
   // create sensor object
   const sensor = sensorObject[sensorName]()
 
-  sensor.addEventListener('reading', () => {
-    xValue.textContent = sensor.x.toFixed(1)
-    yValue.textContent = sensor.y.toFixed(1)
-    zValue.textContent = sensor.z.toFixed(1)
-  })
+  if (sensorName === 'ambient-light-sensor') {
+    sensor.addEventListener('reading', () => {
+      xValue.textContent = sensor.illuminance      
+    })
+  } else {
+    sensor.addEventListener('reading', () => {
+      xValue.textContent = sensor.x.toFixed(1)
+      yValue.textContent = sensor.y.toFixed(1)
+      zValue.textContent = sensor.z.toFixed(1)
+    })
+  }
 
   sensor.addEventListener('error', event => {
     state.textContent = `${event.error.name}, ${event.error.message}`
   })
 
-  sensor.start()  
+  sensor.start()
 }
 
 // starter IIFE
-(() => sensors.forEach((sensorName, index) => {
+(() => sensors.forEach((sensorName, index) => {  
   navigator.permissions.query({ name: sensorName })
     .then(result => {
-       
+
       // check whether sensor presents
       if (result.state === 'denied') {
         console.warn(`Permission to use ${sensorName} sensor is denied.`)
