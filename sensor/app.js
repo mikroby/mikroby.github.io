6c1f530b7feb355d471e5b1d078d4a46b32805f7
mikroby <rtamas@hotmail.com>
2022-03-28 12:57:03 +0200

    more

--- sensor/app.js
+++ sensor/app.js
@@ -1,22 +1,24 @@
 'use strict'
 
-const sensors = ['accelerometer', 'magnetometer', 'gyroscope', 'ambient-light-sensor']
+const sensors = ['accelerometer', 'gyroscope', 'proximity','magnetometer']
 const sensorObject = {
   'magnetometer'() { return new Magnetometer({ frequency: 10 }) }
   ,
   'accelerometer'() { return new Accelerometer({ frequency: 10 }) }
   ,
-  'gyroscope'() { return new Gyroscope() }
+  'gyroscope'() { return new Gyroscope({ frequency: 10 }) }
   ,
-  'ambient-light-sensor'() { return new AmbientLightSensor() }
+  'proximity'() { return new ProximitySensor() }
+  ,
+  'ambient-light-sensor'() { return new AmbientLightSensor({ frequency: 10 }) }
 }
 
 
-const useSensor = (sensorName, index, result) => {
+const useSensor = (sensorName, index, result = 'okay') => {
   const template = `
     <div class="container">
     <h2 class="sensor">${index}. Sensor : ${sensorName}</h2>
-    <small>${result.state}</small>
+    <small>${result}</small>
     <p class="status index${index}">Status : <span></span></p>  
     <p class="x index${index}">Value along X-axis : <span></span></p>
     <p class="y index${index}">Value along Y-axis : <span></span></p>
@@ -36,13 +38,19 @@ const useSensor = (sensorName, index, result) => {
 
   if (sensorName === 'ambient-light-sensor') {
     sensor.addEventListener('reading', () => {
-      xValue.textContent = sensor.illuminance      
+      xValue.textContent = sensor.illuminance.toFixed(2)
     })
-  } else {
+  }
+  if (sensorName==='accelerometer' || sensorName==='magnetometer'){
     sensor.addEventListener('reading', () => {
       xValue.textContent = sensor.x.toFixed(1)
       yValue.textContent = sensor.y.toFixed(1)
       zValue.textContent = sensor.z.toFixed(1)
+    })
+  }
+  if(sensorName === 'proximity'){
+    sensor.addEventListener('reading', () => {
+      xValue.textContent = sensor.distance.toFixed(2)
     })
   }
 
@@ -54,19 +62,20 @@ const useSensor = (sensorName, index, result) => {
 }
 
 // starter IIFE
-(() => sensors.forEach((sensorName, index) => {  
-  navigator.permissions.query({ name: sensorName })
-    .then(result => {
+(() => sensors.forEach((sensorName, index) => {
+  // navigator.permissions.query({ name: sensorName })
+  //   .then(result => {
 
-      // check whether sensor presents
-      if (result.state === 'denied') {
-        console.warn(`Permission to use ${sensorName} sensor is denied.`)
-        return
-      }
+  //     // check whether sensor presents
+  //     if (result.state === 'denied') {
+  //       console.warn(`Permission to use ${sensorName} sensor is denied.`)
+  //       return
+  //     }
 
-      // otherwise use the sensor.
-      useSensor(sensorName, index + 1, result)
+  // otherwise use the sensor.
+  useSensor(sensorName, index + 1)
+  // useSensor(sensorName, index + 1, result)
 
-    }).catch(error => console.warn(error))
+  // }).catch(error => console.warn(error))
 })
 )()
