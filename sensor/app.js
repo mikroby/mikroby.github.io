47c71f92ddd3facefc88348908dda4bb1941a647
mikroby <rtamas@hotmail.com>
2022-03-28 13:52:29 +0200

    all sens

--- sensor/app.js
+++ sensor/app.js
@@ -1,6 +1,9 @@
 'use strict'
 
-const sensors = ['accelerometer', 'proximity', 'gyroscope', 'magnetometer']
+const sensors = [
+  'accelerometer', 'proximity', 'gyroscope', 'magnetometer',
+  'ambient-light-sensor',
+]
 const sensorObject = {
   'magnetometer'() { return new Magnetometer({ frequency: 5 }) }
   ,
@@ -36,23 +39,22 @@ const useSensor = (sensorName, index, result) => {
   // create sensor object
   const sensor = sensorObject[sensorName]()
 
-  if (sensorName === 'ambient-light-sensor') {
-    sensor.addEventListener('reading', () => {
-      xValue.textContent = sensor.illuminance.toFixed(2)
-    })
-  }
-  if (sensorName === 'accelerometer' || sensorName === 'magnetometer') {
-    sensor.addEventListener('reading', () => {
-      xValue.textContent = sensor.x.toFixed(1)
-      yValue.textContent = sensor.y.toFixed(1)
-      zValue.textContent = sensor.z.toFixed(1)
-    })
-  }
-  if (sensorName === 'proximity') {
-    sensor.addEventListener('reading', () => {
-      xValue.textContent = sensor.distance.toFixed(2)
-    })
-  }
+  sensor.addEventListener('reading', () => {
+    switch (sensorName) {
+      case 'ambient-light-sensor':
+        xValue.textContent = sensor.illuminance.toFixed(2)
+        break
+      case 'accelerometer':
+      case 'magnetometer':
+        xValue.textContent = sensor.x.toFixed(1)
+        yValue.textContent = sensor.y.toFixed(1)
+        zValue.textContent = sensor.z.toFixed(1)
+        break
+      case 'proximity':
+        xValue.textContent = sensor.distance.toFixed(2)
+        break
+    }
+  })
 
   sensor.addEventListener('error', event => {
     state.textContent = `${event.error.name}, ${event.error.message}`
@@ -63,18 +65,21 @@ const useSensor = (sensorName, index, result) => {
 
 // starter IIFE
 (() => sensors.forEach((sensorName, index) => {
+
+  console.log(`${index + 1}. ${sensorName}`)
+
   navigator.permissions.query({ name: sensorName })
     .then(result => {
 
-      // check whether sensor presents
+      // check whether sensor allowed
       if (result.state === 'denied') {
         console.warn(`Permission to use ${sensorName} sensor is denied.`)
         return
       }
 
-  // otherwise use the sensor.
-    useSensor(sensorName, index + 1, result.state)
+      // otherwise use the sensor.
+      useSensor(sensorName, index + 1, result.state)
 
-  }).catch(error => console.warn(error))
+    }).catch(error => console.warn('ERROR: ', error))
 })
 )()
