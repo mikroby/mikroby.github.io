51815aee84bbb68e0dee1e0018a83b261e07c35d
mikroby <rtamas@hotmail.com>
2022-12-28 02:19:17 +0100

    v1.4 deploy

--- debris/assets/main.js
+++ debris/assets/main.js
@@ -24,17 +24,20 @@ const CENTER_Y = canvas.height / 2
 const FRICTION = 0.99
 const SPEED_RATIO = 5
 const BOMB_RANGE = (CENTER_X > CENTER_Y ? CENTER_Y : CENTER_X) / 2
-const AWARD = 2000
-const ENEMY_TIMING = 1000
+const ENEMY_TIMING = 1000 // time in ms
+const STOP_TIMING = 5000  // time in ms
+const AWARD = 2000  // points
+const SCORE_HIT = 100 // points
+const SCORE_KILL = 250  // points
 
-// game variables
+// game variables:
 let player, projectiles, enemies, particles, bomb,
   score, counter, numOfBombs,
-  animationId, timeId,
+  animationID, timerID,
   togglePause
 
-const createEnemies = () => {
-  timeId = setInterval(() => {
+const createEnemy = () => {
+  timerID = setInterval(() => {
     if (togglePause) return
 
     const radius = Math.random() * (30 - 4) + 4
@@ -60,7 +63,13 @@ const createEnemies = () => {
   }, ENEMY_TIMING)
 }
 
-const checkBombAward = () => {
+const scoreManager = (increment) => {
+  if (!player) return
+
+  score += increment
+  scoreDisplay.textContent = score
+
+  // check if bomb to be awarded.
   if (score > counter * AWARD) {
     counter++
     numOfBombs++
@@ -69,39 +78,48 @@ const checkBombAward = () => {
   }
 }
 
+const explosionEffect = (x, y, radius, color) => {
+  for (let i = 0; i < radius * 2; i++) {
+    particles.push(new Particle(x, y, Math.random() * 2, color,
+      {
+        x: (Math.random() - 0.5) * (Math.random() * 6),
+        y: (Math.random() - 0.5) * (Math.random() * 6)
+      }
+    ))
+  }
+}
+
 const endGame = () => {
   window.removeEventListener('click', launchProjectile)
   window.removeEventListener('keyup', checkKeyboard)
 
-  // to do: stop animation after player explosion with promise.
-
-  // when promise resolved.
-  {
-    cancelAnimationFrame(animationId)
-    clearInterval(timeId)
+  // stop animation when timeout.
+  const id_5 = setTimeout(() => {
+    clearTimeout(id_5)
+    cancelAnimationFrame(animationID)
+    clearInterval(timerID)
 
     bigScore.textContent = score
     modal.style.display = 'block'
-  }
+  }, STOP_TIMING)
 }
 
 const animate = () => {
-  animationId = requestAnimationFrame(animate)
+  animationID = requestAnimationFrame(animate)
 
   if (togglePause) return
 
+  // with trailing:
   c.fillStyle = 'rgba(0, 0, 0, 0.1)'
-  // c.clearRect(0, 0, canvas.width, canvas.height)
   c.fillRect(0, 0, canvas.width, canvas.height)
+  // with no trailing:
+  // c.clearRect(0, 0, canvas.width, canvas.height)  
 
-  player.draw()
+  if (player) player.draw()
 
   particles.forEach((particle, index) => {
-    if (particle.alpha <= 0) {
-      particles.splice(index, 1)
-    } else {
-      particle.update()
-    }
+    if (particle.alpha <= 0) particles.splice(index, 1)
+    else particle.update()
   })
 
   if (bomb) {
@@ -116,10 +134,9 @@ const animate = () => {
     if (projectile.x + projectile.radius < 0 ||
       projectile.x - projectile.radius > canvas.width ||
       projectile.y + projectile.radius < 0 ||
-      projectile.y - projectile.radius > canvas.height
-    ) {
-      const id = setTimeout(() => {
-        clearTimeout(id)
+      projectile.y - projectile.radius > canvas.height) {
+      const id_1 = setTimeout(() => {
+        clearTimeout(id_1)
         projectiles.splice(index, 1)
       })
     }
@@ -128,59 +145,60 @@ const animate = () => {
   enemies.forEach((enemy, enemyIndex) => {
     enemy.update()
 
-    const dist = Math.hypot(player.x - enemy.x, player.y - enemy.y)
+    if (player) {
+      const dist = Math.hypot(player.x - enemy.x, player.y - enemy.y)
 
-    if (bomb) {
-      if (dist - enemy.radius - bomb.radius < 1) killEnemyByBomb(enemy, enemyIndex)
+      // collision between player and enemy.
+      if (dist - enemy.radius - player.radius < 1) {
+        // create explosion and kill player.
+        sound.demolition()
+        explosionEffect(player.x, player.y, player.radius * 10, player.color)
+        player = null
+        endGame()
+      }
+
+      if (bomb) {
+        // collision between bomb and enemy.
+        if (dist - enemy.radius - bomb.radius < 1) {
+          // create explosion effect.
+          explosionEffect(enemy.x, enemy.y, enemy.radius, enemy.color)
+          // remove enemy, play sound.
+          const id_4 = setTimeout(() => {
+            clearTimeout(id_4)
+            sound.explosion()
+            enemies.splice(enemyIndex, 1)
+          })
+        }
+      }
     }
-
-    // collision between player and enemy.
-    if (dist - enemy.radius - player.radius < 1) endGame()
 
     projectiles.forEach((projectile, projectileIndex) => {
       const dist = Math.hypot(projectile.x - enemy.x, projectile.y - enemy.y)
 
-      // collision between enemy and projectile
+      // collision between enemy and projectile.
       if (dist - enemy.radius - projectile.radius < 1) {
-
-        // create sound
-        const id = setTimeout(() => {
-          clearTimeout(id)
+        // play sound and remove projectile.
+        const id_2 = setTimeout(() => {
+          clearTimeout(id_2)
           sound.explosion()
+          projectiles.splice(projectileIndex, 1)
         })
 
-        // create explosion effect
-        for (let i = 0; i < enemy.radius * 2; i++) {
-          particles.push(new Particle(projectile.x, projectile.y, Math.random() * 2, enemy.color,
-            { x: (Math.random() - 0.5) * (Math.random() * 6), y: (Math.random() - 0.5) * (Math.random() * 6) }))
-        }
+        // create explosion effect.
+        explosionEffect(projectile.x, projectile.y, enemy.radius, enemy.color)
 
         if (enemy.radius - 10 > 5) {
-
-          // increase score
-          score += 100
-          scoreDisplay.textContent = score
-          checkBombAward()
-
-          // shrink enemy
-          gsap.to(enemy, {
-            radius: enemy.radius - 10
-          })
-          // remove projectile
-          const id = setTimeout(() => {
-            clearTimeout(id)
-            projectiles.splice(projectileIndex, 1)
-          })
+          // increase score.
+          scoreManager(SCORE_HIT)
+          // shrink enemy.
+          gsap.to(enemy, { radius: enemy.radius - 10 })
         } else {
-          // increase score
-          score += 250
-          scoreDisplay.textContent = score
-          checkBombAward()
-          // remove enemy and projectile
-          const id = setTimeout(() => {
-            clearTimeout(id)
+          // increase score.
+          scoreManager(SCORE_KILL)
+          // remove enemy.
+          const id_3 = setTimeout(() => {
+            clearTimeout(id_3)
             enemies.splice(enemyIndex, 1)
-            projectiles.splice(projectileIndex, 1)
           })
         }
       }
@@ -188,28 +206,9 @@ const animate = () => {
   })
 }
 
-const killEnemyByBomb = (enemy, enemyIndex) => {
-  // increase score
-  // score += 250
-  // scoreDisplay.textContent = score
-  // checkBombAward()
-
-  // create explosion effect
-  for (let i = 0; i < enemy.radius * 2; i++) {
-    particles.push(new Particle(enemy.x, enemy.y, Math.random() * 2, enemy.color,
-      { x: (Math.random() - 0.5) * (Math.random() * 6), y: (Math.random() - 0.5) * (Math.random() * 6) }))
-  }
-
-  // remove enemy create sound
-  const id = setTimeout(() => {
-    clearTimeout(id)
-    sound.explosion()
-    enemies.splice(enemyIndex, 1)
-  })
-}
-
 const releaseBomb = () => {
   if (!numOfBombs) return
+
   numOfBombs--
   bombDisplay.textContent = numOfBombs
   bomb = new Bomb(CENTER_X, CENTER_Y, 10, 'red', 10)
@@ -224,7 +223,7 @@ const launchProjectile = (event) => {
     x: Math.cos(angle) * SPEED_RATIO,
     y: Math.sin(angle) * SPEED_RATIO
   }
-  // for each mouse click a new projectile launched
+  // for each mouse click a new projectile launched.
   projectiles.push(new Projectile(CENTER_X, CENTER_Y, 5, 'white', velocity))
 }
 
@@ -242,17 +241,19 @@ const init = () => {
   enemies = []
   particles = []
   bomb = null
+
   score = 0
   counter = 1
   numOfBombs = 0
   togglePause = false
   scoreDisplay.textContent = score
   bombDisplay.textContent = numOfBombs
+
   window.addEventListener('click', launchProjectile)
   window.addEventListener('keyup', checkKeyboard)
 }
 
-// starter IIFE
+// starter IIFE.
 (() => {
   awardDisplay.textContent = AWARD
 
@@ -262,7 +263,7 @@ const init = () => {
     modal.style.display = 'none'
     init()
     animate()
-    createEnemies()
+    createEnemy()
   })
 
 })()
