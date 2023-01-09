7d7204501de2e38a37b252f1bf45d4ea2d79a5ec
mikroby <rtamas@hotmail.com>
2023-01-09 23:36:27 +0100

    try

--- assets/index.js
+++ assets/index.js
@@ -1,9 +1,12 @@
+let counter = 0
 const start = (num) => {
-  if (num === 3) {
-    const content = document.querySelector('.content')
+  console.log(num);
+  counter++
+  if (counter === 3) {
+    const content = document.querySelector('#content')
     const spinner = document.querySelector('#spinner')
     content.classList.toggle('hidden')
     spinner.classList.toggle('hidden')
+    console.log(content);
   }
-  console.log(num);
 }
