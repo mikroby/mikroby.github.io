e233fdf2cd9060c08f592209935e77df72d0dae4
mikroby <rtamas@hotmail.com>
2024-05-02 00:17:26 +0200

    refactor

--- hangosbemondo/app.js
+++ hangosbemondo/app.js
@@ -1,3 +1,9 @@
+const assetsURL = "./assets/";
+const signalURL = assetsURL + "mav_szignal.mp3"
+const welcomeSignalURL = assetsURL + "KISS_signal.wav"
+const startImageURL = assetsURL + "start.png"
+const stopImageURL = assetsURL + "stop.png"
+
 const synth = window.speechSynthesis;
 const voices = synth.getVoices();
 
@@ -6,56 +12,64 @@ let options;
 let signal;
 
 const form = document.querySelector("form");
-const button = form.button;
-const image = document.querySelector("img");
+const button = document.querySelector("button");
+const buttonImage = button.querySelector("img");
 const pitchSlider = document.querySelector("#pitch");
 const rateSlider = document.querySelector("#rate");
 
-const playSound = (event) => {
-  event.preventDefault();
+const toggleButton = () => {
+  if (buttonImage.src.includes("start")) {
+    buttonImage.src = stopImageURL;
+    button.removeEventListener("click", playSound);
+    button.addEventListener("click", stopSound);
+  } else {
+    buttonImage.src = startImageURL;
+    button.removeEventListener("click", stopSound);
+    button.addEventListener("click", playSound);
+  }
+};
 
-  if (Object.values(form).some(({ name }) => !form[name].value)) {
+const stopSound = () => {
+  toggleButton();
+  signal.pause();
+  signal = null;
+  synth.cancel();
+}
+
+const playSound = () => {
+  const formValues = Object.values(form).map(({ name }) => name ? form[name].value : null)
+  // omit the value of the button in the form.
+  formValues.pop()
+
+  if (formValues.some(value => !value)) {
     alert("Hiányzó adatok a bemondáshoz. Válassz értéket minden mezőhöz!");
     return;
   }
 
-  if (button.value === "start") {
-    toggleButton();
+  toggleButton();
 
-    const [trainType, verb, time, track] = Object.values(form).map(
-      ({ name }) => form[name].value
-    );
-    const hour = Number(time.slice(0, 2));
-    const minute = Number(time.slice(3, 5));
-    const trackAffix = options.affix[verb];
-    const prep = options.track.find((item) => item.value === track).prep;
+  const [trainType, verb, time, track] = formValues;
+  const hour = Number(time.slice(0, 2));
+  const minute = Number(time.slice(3, 5));
+  const trackAffix = options.affix[verb];
 
-    const text = `${trainType} ${verb} ${hour} óra ${minute} perckor ${prep} ${track} ${trackAffix}`;
+  const sentence = `${trainType} ${verb} ${hour} óra ${minute} perckor ${track} ${trackAffix}`;
 
-    signal = new Audio("mav_szignal.mp3");
-    signal.addEventListener("ended", () => {
-      const message = new SpeechSynthesisUtterance(text);
-      message.rate = rateSlider.value / 100;
-      message.pitch = pitchSlider.value / 100;
-      message.voice = voices.find((item) => item.lang.includes("hu"));
-      message.volume = 1;
-      synth.speak(message);
-      message.addEventListener("end", () => {
-        toggleButton();
-      });
-    });
-    signal.play();
-  } else {
-    toggleButton();
-    signal.pause();
-    synth.cancel();
-  }
+  signal = new Audio(signalURL);
+  signal.addEventListener("ended", () => {
+    const message = new SpeechSynthesisUtterance(sentence);
+    message.rate = rateSlider.value / 100;
+    message.pitch = pitchSlider.value / 100;
+    message.voice = voices.find((item) => item.lang.includes("hu"));
+    message.volume = 1;
+    synth.speak(message);
+    message.addEventListener("end", stopSound);
+  });
+
+  signal.play();
 };
 
-const welcomeSignal = new Audio("KISS_signal.wav");
-welcomeSignal.autoplay = true;
-
-const initializer = (id, array, elementType) => {
+const initializeForm = (id, array, elementType) => {
   const parent = form.querySelector(id);
 
   array.forEach((item) => {
@@ -66,40 +80,36 @@ const initializer = (id, array, elementType) => {
   });
 };
 
-const toggleButton = () => {
-  if (button.value === "start") {
-    button.value = "stop";
-    image.src = "stop.png";
-  } else {
-    button.value = "start";
-    image.src = "start.png";
-  }
-};
-
-fetch("./options.json")
-  .then((data) => data.json())
-  .then((parsed) => {
-    options = parsed;
-
-    initializer("#train-type", options.trainType, "option");
-    initializer("#track", options.track, "option");
-
-    const { rate, pitch } = options.config;
-    pitchSlider.value = pitch;
-    pitchSlider.after(pitch);
-    rateSlider.value = rate;
-    rateSlider.after(rate);
-
-    pitchSlider.addEventListener("input", (event) => {
-      pitchSlider.nextSibling.replaceWith(event.target.value);
-    });
-
-    rateSlider.addEventListener("input", (event) => {
-      rateSlider.nextSibling.replaceWith(event.target.value);
-    });
-
-    form.addEventListener("submit", playSound);
+const initializeSliders = ({ rate, pitch }) => {
+  pitchSlider.value = pitch;
+  pitchSlider.after(pitch);
+  pitchSlider.addEventListener("input", (event) => {
+    pitchSlider.nextSibling.replaceWith(event.target.value);
   });
+
+  rateSlider.value = rate;
+  rateSlider.after(rate);
+  rateSlider.addEventListener("input", (event) => {
+    rateSlider.nextSibling.replaceWith(event.target.value);
+  });
+}
+
+// START IIFE
+(() => {
+  const welcomeSignal = new Audio(welcomeSignalURL);
+  welcomeSignal.autoplay = true;
+
+  fetch("./options.json").then((data) => data.json()).then((parsedData) => {
+    options = parsedData;
+
+    initializeForm("#train-type", options.trainType, "option");
+    initializeForm("#track", options.track, "option");
+
+    initializeSliders(options.config)
+
+    toggleButton()
+  });
+})();
 // TODO:
 // modal az alert helyett.
 // modal a bemondás szövegével
