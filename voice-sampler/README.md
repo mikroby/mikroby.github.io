d5fed7e6a1b1a1c3c9708028bc2a2dac995542df
mikroby <rtamas@hotmail.com>
2026-02-05 01:07:41 +0100

    feat: add control and info, message

---
+++ voice-sampler/README.md
@@ -0,0 +1,5 @@
+# Audio Analyser Experimental
+
+## Useful links:
+
+https://developer.mozilla.org/en-US/docs/Web/API/AnalyserNode
--- voice-sampler/app.js
+++ voice-sampler/app.js
@@ -1,25 +1,30 @@
+const info = document.getElementById("info");
 const message = document.getElementById("message");
 const statusButton = document.getElementById("status-btn");
 
 const scopeBgColor = "black";
-const scopeRayColor = "green";
+const scopeRayColor = "lightgreen";
+
+// 32, 64, 128, 256, 512, 1024, 2048, 4096, 8192, 16384, 32768. Defaults to 2048.
+let sliderValue = 6;
+let FFT_SIZE = 2 ** (sliderValue + 5);
 
 const canvas = document.querySelector("canvas");
 const canvasCtx = canvas.getContext("2d");
 
 const DPR = window.devicePixelRatio || 1;
-const cssWidth = canvas.clientWidth || 300;
-const cssHeight = canvas.clientHeight || 150;
+const cssWidth = canvas.clientWidth;
+const cssHeight = canvas.clientHeight;
 canvas.width = Math.floor(cssWidth * DPR);
 canvas.height = Math.floor(cssHeight * DPR);
 canvas.style.width = cssWidth + "px";
 canvas.style.height = cssHeight + "px";
 canvasCtx.scale(DPR, DPR);
-
 const WIDTH = cssWidth;
 const HEIGHT = cssHeight;
 
-let analyser, bufferLength, dataArray, drawVisual, mediaStream, audioContext;
+let analyser, bufferLength, dataArray, drawVisual, mediaStream,
+  audioContext, sampleRate;
 
 const draw = () => {
   drawVisual = requestAnimationFrame(draw);
@@ -37,8 +42,8 @@ const draw = () => {
   let x = 0;
 
   for (let i = 0; i < bufferLength; i++) {
-    const v = dataArray[i] / 128.0;
-    const y = (v * HEIGHT) / 2;
+    const v = dataArray[i] / 128;
+    const y = v * HEIGHT / 2;
     if (i === 0) canvasCtx.moveTo(x, y);
     else canvasCtx.lineTo(x, y);
     x += sliceWidth;
@@ -48,7 +53,7 @@ const draw = () => {
   canvasCtx.stroke();
 };
 
-const startAudio = () => {
+const startAudio = async () =>
   navigator.mediaDevices
     .getUserMedia({ audio: true })
     .then((stream) => {
@@ -57,15 +62,16 @@ const startAudio = () => {
       // resume in case it's suspended by autoplay policy
       if (audioContext.state === "suspended") audioContext.resume();
 
+      sampleRate = audioContext.sampleRate;
+
       const source = audioContext.createMediaStreamSource(stream);
       analyser = audioContext.createAnalyser();
       source.connect(analyser);
 
-      analyser.fftSize = 2048;
+      analyser.fftSize = FFT_SIZE;
       bufferLength = analyser.frequencyBinCount;
       dataArray = new Uint8Array(bufferLength);
 
-      message.textContent = "Audio started.";
       draw();
     })
     .catch((err) => {
@@ -78,30 +84,44 @@ const startAudio = () => {
         message.textContent = "Error: " + err.message;
       }
     });
-};
-
-// startAudio();
 
 const stopAudio = () => {
   if (drawVisual) cancelAnimationFrame(drawVisual);
   if (mediaStream) mediaStream.getTracks().forEach((t) => t.stop());
   if (audioContext && audioContext.state !== "closed") audioContext.close();
+
+  state = 'stop';
+  message.textContent = "Audio stopped.";
+  statusButton.textContent = 'Start'
 };
 
 let state = 'stop';
 
-statusButton.addEventListener("click", () => {
+statusButton.addEventListener("click", async () => {
   switch (state) {
     case 'stop':
-      startAudio();
+      await startAudio();
       state = 'start';
+      info.textContent = `Sample rate: ${sampleRate / 1000} kHz
+      screen width: ${(bufferLength / sampleRate * 1000).toFixed(3)} ms
+      `;
       message.textContent = "Audio started.";
       statusButton.textContent = 'Stop'
       break
     case 'start':
       stopAudio();
-      state = 'stop';
-      message.textContent = "Audio stopped.";
-      statusButton.textContent = 'Start'
   }
+});
+
+const fftSlider = document.querySelector('#fft-slider');
+
+fftSlider.value = sliderValue;
+fftSlider.nextElementSibling.textContent = FFT_SIZE;
+
+fftSlider.addEventListener('input', (event) => {
+  if (state !== 'stop') stopAudio();
+
+  sliderValue = Number(event.target.value);
+  FFT_SIZE = 2 ** (sliderValue + 5);
+  fftSlider.nextElementSibling.textContent = FFT_SIZE;
 });
